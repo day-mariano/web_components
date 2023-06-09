5c83cf007110025c3bc3a1ad18662db64a26019a
Dayana Mariano <dayanaraquelle@gmail.com>
2023-06-09 16:37:30 -0300

    add text

--- componente.js
+++ componente.js
@@ -8,13 +8,27 @@ class MeuCirculo extends HTMLElement {
     const raiz = document.createElement('span')
     raiz.setAttribute('class', 'raiz')
 
+    const texto = document.createElement('span')
+    texto.setAttribute('class', 'texto')
+    texto.textContent = this.getAttribute("texto")
+
+    raiz.appendChild(texto)
+
     const style = document.createElement('style')
     style.textContent = `
     .raiz {
+      display: flex;
+      align-items: center;
+      justify-content: center;
       width: 200px;
       height: 200px;
-      border-radious: 100%;
-      background-color: red;
+      border-radius: 100%;
+      background-color: ${this.getAttribute("cor") || "red"};
+    }
+
+    .texto {
+      font-size: 2rem;
+      color: white;
     }
     `
     shadow.appendChild(style)
